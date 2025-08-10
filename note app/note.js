a9acdeec4c20711a87d562ca1608f5b2f328a6f1
Sanjam Preet <sanjam.preet@gmail.com>
2025-08-10 00:26:23 -0400

    Added 'click' event listeners and more functionalities

--- note app/note.js
+++ note app/note.js
@@ -4,6 +4,8 @@ let addBtn = document.getElementById("addBtn");
 
 addNote.addEventListener("click", function() {
     document.querySelector(".addForm").style.display="block"
+    document.getElementById("title").value="";
+    document.getElementById("description").value="";
 })
 
 deleteIcon.addEventListener("click", function() {
@@ -14,11 +16,14 @@ addBtn.addEventListener("click", function() {
     document.querySelector(".addForm").style.display="none"
 
     let div = document.createElement("div");
-    div.classList.add("addNote");
+    div.classList.add("myNote");
     div.innerHTML = `
     <h2>${document.getElementById("title").value}</h2>
     <p>${document.getElementById("description").value}</p>
     <button>Delete</button>
     `
+    div.querySelector("button").addEventListener("click", function() {
+        div.remove();
+    })
     document.querySelector(".newNotes").appendChild(div)
 })
