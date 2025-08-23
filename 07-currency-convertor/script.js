f71fa206c20f3d9a07ccf4392b829282310c834c
Sanjam Preet <sanjam.preet@gmail.com>
2025-08-23 12:56:39 -0400

    Added a functionality for currencies

--- 07-currency-convertor/script.js
+++ 07-currency-convertor/script.js
@@ -1,36 +1,38 @@
 const state = {
-    openedDrawer: null
+    openedDrawer: null,
+    currencies: []
 }
 
 // Selectors
 const ui = {
     controls: document.getElementById("controls"),
     drawer: document.getElementById("drawer"),
-    dismissBtn: document.getElementById("dismiss-btn")
+    dismissBtn: document.getElementById("dismiss-btn"),
+    currencyList: document.getElementById("currency-list")
 };
 
 // Event listeners
 const setupEventListeners = () => {
+    document.addEventListener("DOMContentLoaded", initApp);
     ui.controls.addEventListener("click", showDrawer);
     ui.dismissBtn.addEventListener("click", hideDrawer);
 }
 
 // Event handlers
+const initApp = () => {
+    fetchCurrencies()
+}
+
 const showDrawer = (e) => {
     const drawerBtn = e.target.closest("[data-drawer]");
     if(drawerBtn) {
         state.openedDrawer = drawerBtn.id;
         ui.drawer.classList.add("show");
+        console.log(state);
     }
 };
-// const showDrawer = (e) => {
-//     if(e.target.hasAttribute("data-drawer")) {
-//         state.openedDrawer = e.target.id;
-//         ui.drawer.classList.add("show");
-//     }
-// };
 
-const hideDrawer = (e) => {
+const hideDrawer = () => {
     state.openedDrawer = null;
     ui.drawer.classList.remove("show");
     console.log(state);
@@ -41,6 +43,9 @@ const hideDrawer = (e) => {
 // Helper functions
 
 // API functions
+const fetchCurrencies = () => {
+    
+} 
 
 // Initialization
 
