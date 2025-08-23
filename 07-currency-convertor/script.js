79175901f4ef83c2e20fb8fa57cc8d710384529b
Sanjam Preet <sanjam.preet@gmail.com>
2025-08-22 20:32:22 -0400

    Added Selectors in the JS file

---
+++ 07-currency-convertor/script.js
@@ -0,0 +1,19 @@
+// Selectors
+const ui = {
+    controls: document.getElementbyId("controls"),
+    drawer: document.getElementById("drawer"),
+    dismissBtn: document.getElementById("dismiss-btn")
+};
+
+// Event listeners
+
+
+// Event handlers
+
+// Render functions
+
+// Helper functions
+
+// API functions
+
+// Initialization
