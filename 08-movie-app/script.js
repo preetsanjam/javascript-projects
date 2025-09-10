a4e79419d935143a308ddcb8f9630852c7aa8efc
Sanjam Preet <sanjam.preet@gmail.com>
2025-09-10 01:13:23 -0400

    Created three variables in JS file

--- 08-movie-app/script.js
+++ 08-movie-app/script.js
@@ -1,3 +1,6 @@
 // Titles: https://www.omdbapi.com/?s=thor&page=1&apikey=50d32271
 // Details: http://www.omdbapi.com/?i=tt3896198&apikey=50d32271
 
+const movieSearchBox = document.getElementById("movie-search-box");
+const searchList = document.getElementById("search-list");
+const resultGrid = document.getElementById("result-grid");
