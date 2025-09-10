5216bf75d8d01ef16ff7c3f34356614174b9ea69
Sanjam Preet <sanjam.preet@gmail.com>
2025-09-10 17:16:40 -0400

    Added async() functions to JS file

--- 08-movie-app/script.js
+++ 08-movie-app/script.js
@@ -4,3 +4,18 @@
 const movieSearchBox = document.getElementById("movie-search-box");
 const searchList = document.getElementById("search-list");
 const resultGrid = document.getElementById("result-grid");
+
+// Load movie titles from API
+async function loadMovies(searchTerm) {
+    const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=50d32271`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    // console.log(data.Search);
+    if (data.Response === "True") displayMovieList(data.Search); 
+}
+
+function findMovies() {
+    // trim() removes whitespace (spaces, tabs, line breaks) from the beginning and end of a string
+    let searchTerm = (movieSearchBox.value).trim(); 
+    console.log(searchTerm);
+}
