d4ac0913ca38f36cecd5c858dd642321990abc9d
Sanjam Preet <sanjam.preet@gmail.com>
2025-09-11 21:03:34 -0400

    Added a few comments on idx=0

--- 08-movie-app/script.js
+++ 08-movie-app/script.js
@@ -27,9 +27,13 @@ function findMovies() {
 
 function displayMovieList(movies) {
     searchList.innerHTML = "";
-    for(let idx = 0; idx < movies.length; idx++) { // let idx = 0 → This creates the counter variable idx;  sets its starting value to 0
+    for(let idx = 0; idx < movies.length; idx++) { // let idx = 0 → This creates the counter variable idx;  sets its starting value to 0        
                                                     // idx < movies.length → The loop will run as long as this condition is true
                                                     // idx++ → After each loop cycle, idx increases by 1
+                                                    // Summary:
+                                                    // Start at the first movie (index 0)
+                                                    // Keep looping until the last movie
+                                                    // Move forward one step at a time
 
         let movieListItem = document.createElement(`div`);
         movieListItem.dataset.id = movies[idx].imdbID;
